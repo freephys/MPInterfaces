91e0c877b366e21ce16626dee1e78c649eb07e21
JARVIS-Unifies <writetokamal.1989@gmail.com>
2017-02-06 11:10:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ MPINT_DIR = os.path.dirname(os.path.abspath(__file__))
 setup(
     name = "mpinterfaces",
     version = "1.1.2",
-    install_requires=["pymatgen>=3.2.7", "FireWorks>=1.2.2",
+    install_requires=["pymatgen==3.4.1", "FireWorks>=1.2.2",
                       "custodian>=0.8.8", "pymatgen-db>=0.5.1",
                       "ase>=3.9.1"],
     extras_require={"plot": ["matplotlib>=1.4.2"],
